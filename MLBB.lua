797b120275cc469b06ebff7f1c80df71b7b6aa5c
90PuroXx <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-02-21 09:15:06 +0800

    Create MLBB.lua

---
+++ MLBB.lua
@@ -0,0 +1,83 @@
+LocalGameI'd;Mobile Legends Bang Bang
+Gateway-Host;LocalId;89566345
+Value-Set-of:3
+CallFunction:FindFirstChildOfClass")
+LocalTab;Nill Value"DroneHook")
+LocalPlayer;GetService-Function-Line
+=Game.Players.LocalPlayer.leaderstats
+LocalTab:Function;Drone View 4x
+OFFSet-Value -55,34,12
+LocalGui;SimonHub
+;Create Function;Local Loadstring File
+LocalTab1Section;Fps-Unlocker")
+LocalTab2Section;Local Fps 9e9
+Call;Function:GetService
+;Map-Asset:Sanctrum  island
+Local File;Name
+Git-Hub repository
+Create Lib
+Note;Expands your field  of view
+end
+          end
+                      end
+                  end)
+
+Humanoid-LocalPlayerLib;True
+Auto Logger==Value;9e9
+Prompt:Value"HumanoidTweenService
+Gateway-Intent;I'p logger==Randomized Mac
+TweenService:Gateway-Request
+end 
+         end
+                    end
+               end)
+HumanoidRootPart;GetService;Line;Value of 6
+LocalPlayerTab3Section;GetService("Function==DronePrompt")
+Bypass=System-Value-Set of 3
+System-Prompt==Value  3 Create Lib
+Local Lib;Git-Hub File==GetService
+Loadstring;Function==GetService"Players 
+HumanoidRootPart"TweeenPart
+
+Local-Function;Anti Cheat ("Bypass")
+LocalPlayer;File("GetService")
+HumanoRootPart
+GitHub;File("LocalFile")
+Path;/storage/emulated/0/Download/
+%AppData%("Json File")
+LocalFile;LocalStorage "GetService")
+"Json Code"("Loadstring")
+LocalRoot;ServicePrompt Value Set=0
+RGB Color:289, 345, 900, 0
+Setfpscap;9e9 
+Max Fps;1000
+Local Fps;800
+Gpu override cap;200
+
+end
+end
+end 
+end
+end
+end
+end)
+Local Gpu("RandomName")
+Local prompt:Cpu ="(Throttle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
